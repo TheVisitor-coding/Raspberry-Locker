cc656663cd56fa760b9c064e50bd792b46fe0f48
MatteoRossi_Dev <designbymatrossi@gmail.com>
2023-09-05 14:09:43 +0200

    script take data

--- index.py
+++ index.py
@@ -1,1 +1,18 @@
-uuu
+from urllib.request import urlopen
+  
+# import json
+import json
+# store the URL in url as 
+# parameter for urlopen
+url = "http://10.54.130.221/locker/test.json"
+  
+# store the response of URL
+data = urlopen(url)
+  
+# storing the JSON response 
+# from url in data
+data_json = json.loads(data.read())
+
+  
+# print the json response
+print(data_json)
